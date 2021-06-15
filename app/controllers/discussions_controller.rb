de5630d9ed0d3becc3ac3b1f1c73ede5c89fb619
Lee Stetson <stetsonthree@icloud.com>
2021-06-15 11:23:51 -0400

    Add new post when creating a discussion

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -12,6 +12,7 @@ class DiscussionsController < ApplicationController
 
   def new
     @discussion = Discussion.new
+    @discussion.posts.new
   end
 
   def create
@@ -49,7 +50,7 @@ class DiscussionsController < ApplicationController
   private
 
   def discussion_params
-    params.require(:discussion).permit(:name, :closed, :pinned)
+    params.require(:discussion).permit(:name, :closed, :pinned, posts_attributes: :body)
   end
 
   def set_discussion
