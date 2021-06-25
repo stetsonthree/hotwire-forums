8278c3baeee900f98bab8d4871d6324310f11c05
Lee Stetson <stetsonthree@icloud.com>
2021-06-25 18:15:17 -0400

    Realtime deletion of posts

--- app/controllers/discussions/posts_controller.rb
+++ app/controllers/discussions/posts_controller.rb
@@ -2,7 +2,7 @@ module Discussions
   class PostsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_discussion
-    before_action :set_post, only: [:show, :edit, :update]
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
 
     def show
 
@@ -33,6 +33,15 @@ module Discussions
           format.turbo_stream
           format.html { render :new, status: :unprocessable_entity }
         end
+      end
+    end
+
+    def destroy
+      @post.destroy
+
+      respond_to do |format|
+        format.turbo_stream ( ) # le tthe callback delete the post
+        format.html { redirect_to @post.discussion, notice: "Post deleted" }
       end
     end
 
