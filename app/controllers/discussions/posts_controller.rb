912b738dd0df506cf3039297efcd364fcb68658a
Lee Stetson <stetsonthree@icloud.com>
2021-06-25 18:10:03 -0400

    Edit posts and add realtime updates for everyone

--- app/controllers/discussions/posts_controller.rb
+++ app/controllers/discussions/posts_controller.rb
@@ -2,6 +2,26 @@ module Discussions
   class PostsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_discussion
+    before_action :set_post, only: [:show, :edit, :update]
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to @post.discussion, notice: "Post updated" }
+        else
+          format.turbo_stream
+          format.html { render :edit, status: :unprocessable_entity }
+        end
+      end
+    end
 
     def create
       @post = @discussion.posts.new(post_params)
@@ -20,6 +40,10 @@ module Discussions
 
     def set_discussion
       @discussion = Discussion.find(params[:discussion_id])
+    end
+
+    def set_post
+      @post = @discussion.posts.find(params[:id])
     end
 
     def post_params
